53f22724f6158b20c3ffaae08b9dc286e57834f6
Antonio Goncalves <antonio.goncalves@gmail.com>
2023-08-17 12:52:20 +0200

    Config server usage should be reported on for spring boot apps (#128)
    * config server usage should be reported on for spring boot apps
    
    * Bump to WindUp 6.3.0.Final (#143)
    
    * Create a GitHub workflow to add a triage label (#145)
    
    * Bump Maven dependency plugin and SLF4J (#147)
    
    * Change the messages
    
    * Fix URL

---
+++ rules/rules-reviewed/azure/springboot/tests/data/spring-boot-to-azure-config-server/ConfigServer.java
@@ -0,0 +1,7 @@
+import org.springframework.cloud.config.server.EnableConfigServer.EnableConfigServer;
+
+@EnableConfigServer
+public class ConfigServer {
+
+    String notHomePath = "blah blah blah";
+ }
