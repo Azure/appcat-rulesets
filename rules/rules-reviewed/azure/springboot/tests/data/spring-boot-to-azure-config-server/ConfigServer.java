d86f66be472f86eaa165c7b336807e81e0ad4979
kthatipally <99229993+kthatipally@users.noreply.github.com>
2024-03-13 16:23:49 -0500

    Revert "Merge additional rules updates from main to release-6.3.z branch (#188)"
    This reverts commit 27c7c589f2e0524f46d12ea7fce6493aa8b00f86.

--- rules/rules-reviewed/azure/springboot/tests/data/spring-boot-to-azure-config-server/ConfigServer.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.cloud.config.server.EnableConfigServer.EnableConfigServer;
-
-@EnableConfigServer
-public class ConfigServer {
-
-    String notHomePath = "blah blah blah";
- }
