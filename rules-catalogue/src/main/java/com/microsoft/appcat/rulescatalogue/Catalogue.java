4e68c3ccb8e392d176e9f54db72bf031197458a0
Bruno Borges <brborges@microsoft.com>
2024-07-12 19:46:23 -0700

    Make it work with JDK 17

--- rules-catalogue/src/main/java/com/microsoft/appcat/rulescatalogue/Catalogue.java
+++ rules-catalogue/src/main/java/com/microsoft/appcat/rulescatalogue/Catalogue.java
@@ -2,6 +2,28 @@ package com.microsoft.appcat.rulescatalogue;
 
 import javax.xml.transform.stream.StreamSource;
 
-public record Catalogue(String name, String directoryPath, StreamSource xsltTemplate) {
+public class Catalogue {
+
+    private final String name;
+    private final String directoryPath;
+    private final StreamSource xsltTemplate;
+
+    public Catalogue(String name, String directoryPath, StreamSource xsltTemplate) {
+        this.name = name;
+        this.directoryPath = directoryPath;
+        this.xsltTemplate = xsltTemplate;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String directoryPath() {
+        return directoryPath;
+    }
+
+    public StreamSource xsltTemplate() {
+        return xsltTemplate;
+    }
 
 }
