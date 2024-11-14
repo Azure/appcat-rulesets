58bf718c4003c5c8312f58c2dac2cb3c1514b646
Keerthi Thatipally <99229993+kthatipally@users.noreply.github.com>
2024-11-14 15:39:08 -0600

    Revert "Get updates from upstream 6.3.9.Final and upgrade AppCAT base version from 6.3.0 to 6.3.9"

--- rules/rules-reviewed/eap8/eap7/tests/data/log4j-removed-no-config/Test.java
+++
@@ -1,8 +0,0 @@
-import org.apache.log4j.Logger;
-
-public class Test {
-
-    public static void main(String[] args) {
-        Logger logger = new Logger();
-    }
-}
--- rules/rules-reviewed/eapxp/eapxp5/tests/data/data-eapxp_microprofile_metrics_not_supported/MicroProfileApplication.java
+++
@@ -1,5 +0,0 @@
-package org.jboss.eap.xp.test;
-
-import org.eclipse.microprofile.metric.*;
-public class MicroProfileApplication {
-}
